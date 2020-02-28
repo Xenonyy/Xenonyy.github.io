02e8da42654d9ec4131db7923a5f98693a305eba
Xenon <gonda.armand@gmail.com>
2020-02-28 13:19:32 +0100

    Working on replacing the huge div mess in HTML for the loader with JS.

--- web.js
+++ web.js
@@ -43,3 +43,17 @@ $("h3").click(async function () {
     await loadAnimation(400, schoolpage);
     changeBG('js.jpg');
 });
+
+
+// TODO: DOESNT CREATE THE ELEMENT FOR SOME REASON. NOT EVEN IN HTML..........
+
+
+// for (let i = 0; i < 5; i++) {
+//     const spinner = document.createElement("div", { class: "spinner"});
+//     const spinnerContainer = document.createElement("div", { class: "spinnerContainer"});
+//     const hex0 = document.createElement("div", { class: "hex0"});
+//     const hex120 = document.createElement("div", { class: "hex120"});
+//     const hex240 = document.createElement("div", { class: "hex240"});
+// }
+// $(".loaderContainer").append($(".spinner"));
+// $(".spinnerContainer").appendTo($(".spinner"));
