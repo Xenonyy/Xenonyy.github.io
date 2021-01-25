8fe37fe6b2f2716c782536e38c1bc5ce1f7142e7
Xenon <gonda.armand@gmail.com>
2021-01-25 15:57:27 +0100

    Removed more unused code, rewrote on page load function.

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,67 +1,11 @@
-const body = $("body"),
-    homepage = $("#homePage"),
-    schoolpage = $("#schoolPage"),
-    aboutpage = $("#aboutPage"),
-    contactpage = $("contactPage"),
-    pages = $(".page"),
+const homepage = $("#homePage"),
     d = document;
 
-body.removeClass("stop-scrolling");
-function homeAni() {
+window.onload = () => {
     homepage.fadeIn(2000);
     homepage.css({"display": "inline-table"});
+    location.assign("#home");
 }
-homeAni();
-// Setting up homepage and the loading screen	
-// $(window).on("load", async function () {	
-//     await loadAnimation(500, homepage);
-//     window.location.assign("https://xenonyy.github.io/home");
-// });
-
-// Remove the scrollbar to wait for the loading animation
-// $(window).on('load', function () {
-//     setTimeout(() => { $('body').removeClass('stop-scrolling'); }, 750);
-// });
-
-// Navigation in between pages
-// async function loadAnimation(ms, page) {
-//     pages.slideUp(ms);
-//     return new Promise(resolve => setTimeout(resolve, ms))
-//         .then(() => page.slideDown(600));
-// }
-// function changeBG(source) {	
-//     document.body.style.backgroundImage = "url(" + source + ")";	
-// }
-
-// $("#home").click(async function () {
-    // await loadAnimation(400, homepage);
-    // changeBG('abstract.jpg');
-// });
-// $("h2").click(async function () {
-    // await loadAnimation(400, aboutpage);
-    // changeBG('triangle.jpg');
-// });
-// $("h3").click(async function () {
-    // await loadAnimation(400, schoolpage);
-    // changeBG('js.jpg');
-// });
-
-
-// Navigation
-$("#home").on("click", function () {
-    d.getElementById("homePage").scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
-});
-$("#aboutMe").on("click", function () {
-    d.getElementById("aboutPage").scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
-    // location.assign("/about")
-});
-$("#contact").click(function () {
-    d.getElementById("contactPage").scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
-});
-$("#projects").click(function () {
-    d.getElementById("projectsPage").scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
-});
-
 
 // Animations
 function aboutAni() {       
