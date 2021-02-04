c44f169ce3a22827a04e875a29e9caab3d7aa332
Xenon <gonda.armand@gmail.com>
2021-02-04 23:13:34 +0100

    Made the website able to be run locally.

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http'),
+    hostname = 'localhost',
+    port = 3000,
+    express = require('express'),
+    app = express();
+
+app.use(express.static(__dirname));
+app.get('/', (req,res) => {
+    res.sendStatus(200);
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+//mount other paths
+// in this case render 404 for any other path than '/'
+app.get("*",function (req, res) {
+    res.status(404).send('<h1>File not found</h1>');
+    res.end();
+});
+  
+http.createServer(app).listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
